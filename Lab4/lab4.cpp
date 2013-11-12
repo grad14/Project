d9dbe75a97e9ce35759fc271a8e9483666f9d458
Anber Ikram <anberikram@yahoo.com>
2013-11-11 21:20:45 -0500

    Test1

---
+++ Lab4/lab4.cpp
@@ -0,0 +1,23 @@
+/*
+ * lab4.cpp
+ *
+ *  Created on: Nov 11, 2013
+ *      Author: Rahim
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+	double r = 5.0;
+	double circle;
+
+	circle = 2 * r;
+	cout << circle;
+
+return 0;
+
+}
+
+
