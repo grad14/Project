a5a75f64122a7c23c2fc21e829df624b92a66206
Anber Ikram <anberikram@yahoo.com>
2013-11-24 18:47:05 -0500

    Changes to Lab 4!

--- Lab4/lab4.hpp
+++ Lab4/lab4.hpp
@@ -8,44 +8,70 @@
 #ifndef LAB4_HPP_
 #define LAB4_HPP_
 
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+
+using namespace std;
+
 class MedImage{
 
-public:
-// Data members:
-	//image type
-	//samples per pixel
-	//bits allocated
-	//photometric interpretation
-// Constructors:
-	MedImage();
+protected:
+	string imagetype; 	// attribute: image type
+	int samperpix; 		// attribute: samples per pixel
+	int bitall; 		// attribute: bits allocated
+	string photoint; 	// attribute: photometric interpretation
+	string image; 		// variable to determine type of image
 
-// Functions:
-	void checkres(); //launch external image viewer to display image
-	void appendinfo(); // append info of medical-image chart to patient file
+public:
+	string name; char id;
+	MedImage();
+	void addannot(string name, char id); 	// MedImage contains annotation/comment - name/id associated with it
+	void seeimg(); 							// launch external image viewer to display image i.e. checks results of class MedImage
+	void typeofimage(string image); 		// function allows radiologist to specify type of image
+	void storeinfo(); 						// append info of medical-image chart to patient file (array)
+	// complete storeinfo function
 };
 
+
 class MRI : public MedImage {
-	//echo time
-	//echo number
-	//imaging frequency
-	//magnetic field strength
-	//SAR
+
+private:
+	int echotime; 		// echo time
+	int echonum; 		// echo number
+	int imagfreq; 		// imaging frequency
+	int magfstren; 		// magnetic field strength
+	int sar; 			// SAR
+
+public:
+	MRI(); 				// use initilization list to specify common attributes
+
 };
 
 class US : public MedImage {
-	//number of stages
-	//view number
-	//trigger time
-	//focus depth
-	//lesion number
+
+private:
+	int nominterval; 	// nominal interval
+	int viewnum; 		// view number
+	int trigtime; 		// trigger time
+	int focdepth; 		// focus depth
+	int lesnum; 		// lesion number
+
+public:
+	US(); 				// use initilization list to specify common attributes
 };
 
 class CT : public MedImage {
-	//exposure time
-	//generator power
-	//x-ray tube current
-	//distance source-to-detector
-	//distance source-to-patient
+
+private:
+	int expotime; 		// exposure time
+	int genpow;	 		// generator power
+	int curr; 			// x-ray tube current
+	int dissourtodet; 	// distance source-to-detector
+	int dissourtopat; 	// distance source-to-patient
+
+public:
+	CT(); 				// use initilization list to specify common attributes
 };
 
 #endif /* LAB4_HPP_ */
---
+++ Lab4/lab41.cpp
@@ -0,0 +1,60 @@
+/*
+ * lab41.cpp
+ *
+ *  Created on: Nov 18, 2013
+ *      Author: Rahim
+ */
+
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include "lab4.hpp"
+
+using namespace std;
+
+void MedImage :: addannot(string name, char id){
+	cout << "Please enter your name and id";
+	cin >> name >> id;
+
+	string mystring;
+	cout << "Enter your comment below";
+	getline (cin, mystring);
+};
+
+void MedImage :: seeimg(){
+	system("start gimp-2.8.exe MRI.jpg");
+};
+
+void MRI :: MRI(){
+	// FILL IN INITIALIZATIONS
+};
+
+void US :: US(){
+	// FILL IN INITIALIZATIONS
+};
+
+void CT:: CT(){
+	// FILL IN INITIALIZATIONS
+};
+
+void typeofimage(string image){
+	cout << "Please specify if the image is an MRI, US, or CT image:";
+	cin >> image;
+
+switch(image){
+
+case 'MRI':
+	MRI mri;
+	break;
+
+case 'US':
+	US us;
+	break;
+
+case 'CT':
+	CT ct;
+	break;
+}
+
+};
+
