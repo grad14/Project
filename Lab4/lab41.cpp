0678cabf0edb468cdbd52a4392358446a460113c
Anber Ikram <anberikram@yahoo.com>
2013-11-24 23:42:27 -0500

    Changes to constructors!

--- Lab4/lab41.cpp
+++ Lab4/lab41.cpp
@@ -25,25 +25,36 @@ void MedImage :: seeimg(){
 	system("start gimp-2.8.exe MRI.jpg");
 };
 
-MRI :: MRI(){
+MRI :: MRI(int echotime, int echonum, int imagfreq, int magfstren, int sar){
 	imagetype = 'Type 1';
 	samperpix = 1;
 	bitall = 16;
 	photoint = 'MONOCHROME1' || 'MONOCHROME2';
+
+	cout << "Please enter the values for the following attributes:\n";
+	cin >> "Echo time:\t" >> echotime >> "Echo Number:\t" >> echonum >> "Image frequency:\t" >> imagfreq >> "Magnetic Field Strength:\t" >> magfstren >> "SAR:\t" >> sar;
 };
 
-US :: US(){
+US :: US(int nominterval, int viewnum, int trigtime, int focdepth, int lesnum){
 	imagetype = 'Type 2';
-	//samperpix = 1;
-	//bitall = 16;
+	if (photoint == 'MONOCHROME2' || 'PALETTE COLOR')
+		{samperpix = 1;}
+	else {samperpix = 3;}
+	bitall = 8;
 	photoint = 'PALETTE COLOR' || 'MONOCHROME2' || 'RGB' || 'YBR_FULL' || 'YBR_FULL_422' || 'YBR_PARTIAL_422' || 'YBR_RCT' || 'YBR_ICT' || 'YBR_PARTIAL_420';
+
+	cout << "Please enter the values for the following attributes:\n";
+	cin >> "Nominal Interval:\t" >> nominterval >> "View Number:\t" >> viewnum >> "Trigger Time:\t" >> trigtime >> "Focus Depth:\t" >> focdepth >> "Lesion Number:\t" >> lesnum;
 };
 
-CT:: CT(){
+CT:: CT(int expotime, int genpow, int curr, int dissourtodet, int dissourtopat){
 	imagetype = 'Type 1';
 	samperpix = 1;
 	bitall = 16;
 	photoint = 'MONOCHROME1' || 'MONOCHROME2';
+
+	cout << "Please enter the values for the following attributes:\n";
+	cin >> "Exposure time:\t" >> expotime >> "Generator Power:\t" >> genpow >> "Current:\t" >> curr >> "Distance-source-to-dectector:\t" >> dissourtodet >> "Distance-source-to-patient:\t" >> dissourtopat;
 };
 
 int MedImage :: typeofimage(int image){
